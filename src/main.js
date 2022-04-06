a7934ebd89acfc40f847e9b3d891d56f84930aa9
s4timuen <tim.muenzinger@yahoo.de>
2022-04-06 17:15:34 +0200

    Added bootstrap, bootstrap-vue-3 (alpha) and required dependency popperjs/core.

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import App from '@/App.vue';
 import router from '@/router/router.js';
 import store from '@/store/store.js';
 import i18n from '@/localization/i18n.js';
+import BootstrapVue3 from 'bootstrap-vue-3'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
 app.use(i18n);
+app.use(BootstrapVue3);
 app.mount('#app');
